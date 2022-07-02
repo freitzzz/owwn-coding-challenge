db8ec1db237e494fa2891a74e7aa92e9f431041c
freitzzz <1160907@isep.ipp.pt>
2022-07-02 21:34:35 +0100

    provide app bloc as top level bloc

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,5 +1,6 @@
 import 'package:owwn_coding_challenge/blocs/blocs.dart';
 import 'package:owwn_coding_challenge/core/core.dart';
+import 'package:owwn_coding_challenge/data/data.dart';
 import 'package:owwn_coding_challenge/presentation/presentation.dart';
 
 class OWWNCodingApp extends StatelessWidget {
@@ -14,16 +15,23 @@ class OWWNCodingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return RepositoryProvider<Vault>(
       create: (context) => vault,
-      child: MaterialApp(
-        title: 'Flutter Coding Challenge',
-        theme: owwnCodingLightTheme,
-        darkTheme: owwnCodingDarkTheme,
-        themeMode: ThemeMode.dark,
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-        ],
-        supportedLocales: supportedLocales,
-        home: appNavigator,
+      child: BlocProvider(
+        lazy: false,
+        create: (context) => AppBloc(
+          deviceRepository:
+              vault.lookup<DeviceRepository>() as DeviceRepository,
+        )..add(AppStarted()),
+        child: MaterialApp(
+          title: 'Flutter Coding Challenge',
+          theme: owwnCodingLightTheme,
+          darkTheme: owwnCodingDarkTheme,
+          themeMode: ThemeMode.dark,
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+          ],
+          supportedLocales: supportedLocales,
+          home: appNavigator,
+        ),
       ),
     );
   }
