3d9335fd44c3e507053707fd9e7ad0438d3cafc1
freitzzz <1160907@isep.ipp.pt>
2022-07-02 15:36:22 +0100

    changed authentication repository api implemented OWWN authentication repository

--- lib/data/repositories/authentication/authentication_repository.dart
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -1,32 +1,111 @@
 import 'package:dartz/dartz.dart';
+import 'package:owwn_coding_challenge/core/core.dart';
+import 'package:owwn_coding_challenge/data/data.dart';
 import 'package:owwn_coding_challenge/models/models.dart';
 
 abstract class AuthenticationRepository {
-  Future<Either<AuthenticationError, void>> auth({
-    required final String email,
+  Future<Either<AuthenticationError, Session>> auth({
+    required final Credentials credentials,
   });
 
-  Future<Either<AuthenticationError, void>> refresh({
-    required final String token,
-  });
+  Future<Either<AuthenticationError, Session>> refresh();
 }
 
 class FakeAuthenticationRepository extends AuthenticationRepository {
+  final _fakeSession = {
+    'access_token': 'fake_jwt',
+    'refresh_token': 'fake_jwt',
+  };
+
   @override
-  Future<Either<AuthenticationError, void>> auth({
-    required String email,
+  Future<Either<AuthenticationError, Session>> auth({
+    required Credentials credentials,
   }) {
     return Future.value(
-      const Right(null),
+      Right(
+        Session.fromJson(_fakeSession),
+      ),
     );
   }
 
   @override
-  Future<Either<AuthenticationError, void>> refresh({
-    required String token,
-  }) {
+  Future<Either<AuthenticationError, Session>> refresh() {
     return Future.value(
-      const Right(null),
+      Right(
+        Session.fromJson(_fakeSession),
+      ),
+    );
+  }
+}
+
+class OWWNAuthenticationRepository extends AuthenticationRepository {
+  final OWWNCodingNetworkingClient client;
+
+  late Session session;
+
+  OWWNAuthenticationRepository({
+    required this.client,
+  });
+
+  @override
+  Future<Either<AuthenticationError, Session>> auth({
+    required Credentials credentials,
+  }) {
+    return safeAsyncThrowCall(
+      () async {
+        final response = await client.post(
+          endpoint: authEndpoint,
+          data: credentials.toJson().encode,
+        );
+
+        final sessionFold = _foldSessionResponse(response);
+        session = sessionFold.getOrElse(() => session);
+
+        return sessionFold;
+      },
+    );
+  }
+
+  @override
+  Future<Either<AuthenticationError, Session>> refresh() {
+    return safeAsyncThrowCall(
+      () async {
+        final response = await client.post(
+          endpoint: refreshEndpoint,
+          data: session.toRefreshJson().encode,
+        );
+
+        final sessionFold = _foldSessionResponse(response);
+        session = sessionFold.getOrElse(() => session);
+
+        return sessionFold;
+      },
+    );
+  }
+
+  Either<AuthenticationError, Session> _foldSessionResponse(
+    final Either<RequestError, Response> response,
+  ) {
+    return response.fold(
+      (l) => Left(AuthenticationError.from(l)),
+      (r) {
+        if (r is JsonResponse) {
+          return Right(
+            Session.fromJson(r.json),
+          );
+        } else if (r is ErrorResponse) {
+          return Left(
+            AuthenticationError.parse(r),
+          );
+        } else {
+          return Left(
+            UnknownAuthenticationError(
+              stacktrace: StackTrace.current,
+              cause: 'could not parse $r response',
+            ),
+          );
+        }
+      },
     );
   }
 }
