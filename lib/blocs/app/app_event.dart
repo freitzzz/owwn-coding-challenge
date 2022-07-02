a08754ae13b353c098875aab0e4bc2d65235f5bb
freitzzz <1160907@isep.ipp.pt>
2022-07-02 22:02:58 +0100

    defined app authenticated and refresh session started events

--- lib/blocs/app/app_event.dart
+++ lib/blocs/app/app_event.dart
@@ -3,3 +3,14 @@ part of 'app_bloc.dart';
 abstract class AppEvent {}
 
 class AppStarted extends AppEvent {}
+
+class AppAuthenticated extends AppEvent {
+  final Session session;
+
+  AppAuthenticated({
+    required this.session,
+  });
+}
+
+@visibleForTesting
+class RefreshSessionStarted extends AppEvent {}
