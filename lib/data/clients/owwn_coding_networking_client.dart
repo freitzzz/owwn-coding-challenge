f6817131ae78c8c0bb5e0d1997b2002c29808291
freitzzz <1160907@isep.ipp.pt>
2022-07-02 15:37:12 +0100

    make sure api key is sent on each owwn coding api request

--- lib/data/clients/owwn_coding_networking_client.dart
+++ lib/data/clients/owwn_coding_networking_client.dart
@@ -1,5 +1,10 @@
-import 'package:http/http.dart';
+import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' hide Request, Response;
 import 'package:networking/networking.dart';
+
+const authEndpoint = '/auth';
+const refreshEndpoint = '/refresh';
+const usersEndpoint = '/users';
 
 class OWWNCodingNetworkingClient extends NetworkingClient {
   OWWNCodingNetworkingClient()
@@ -7,4 +12,20 @@ class OWWNCodingNetworkingClient extends NetworkingClient {
           baseUrl: Uri.parse('https://ccoding.owwn.com/hermes'),
           httpClient: Client(),
         );
+
+  @override
+  Future<Either<RequestError, Response>> send({
+    required final Request request,
+  }) {
+    return super.send(
+      request: request.copyWith(
+        headers: request.headers
+          ..addAll(
+            {
+              'X-API-KEY': 'owwn-challenge-22bbdk',
+            },
+          ),
+      ),
+    );
+  }
 }
