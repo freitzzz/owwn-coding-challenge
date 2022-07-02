d563034a3a28e6aa827899e5dd227ba9dd52f6e2
freitzzz <1160907@isep.ipp.pt>
2022-07-02 14:09:30 +0100

    needed authentication errors models for owwn coding api

--- lib/models/errors/authentication_error.dart
+++ lib/models/errors/authentication_error.dart
@@ -5,4 +5,31 @@ abstract class AuthenticationError extends RequestError {
     required String cause,
     required StackTrace stacktrace,
   }) : super(cause: cause, stackTrace: stacktrace);
+
+  static AuthenticationError from(RequestError error) {
+    if (error is AuthenticationError) {
+      return error;
+    } else {
+      return UnknownAuthenticationError(
+        cause: error.cause,
+        stacktrace: error.stackTrace,
+      );
+    }
+  }
+}
+
+class InvalidCredentialsAuthenticationError extends AuthenticationError {
+  const InvalidCredentialsAuthenticationError({
+    required StackTrace stacktrace,
+  }) : super(cause: 'Invalid Credentials', stacktrace: stacktrace);
+}
+
+class UnknownAuthenticationError extends AuthenticationError {
+  UnknownAuthenticationError({
+    final String? cause,
+    required StackTrace stacktrace,
+  }) : super(
+          cause: 'Unknown Error on Authentication: ${cause?.toString() ?? ''}',
+          stacktrace: stacktrace,
+        );
 }
