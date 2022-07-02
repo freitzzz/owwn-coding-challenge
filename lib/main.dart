5d7604b4a6739c11c8c9ba75f3584ef2db339cab
freitzzz <1160907@isep.ipp.pt>
2022-07-02 12:48:28 +0100

    build app widget on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:owwn_coding_challenge/blocs/blocs.dart';
+import 'package:owwn_coding_challenge/core/core.dart';
+import 'package:owwn_coding_challenge/logging/logging.dart';
 
 void main() {
-  runApp(const MyApp());
+  putLumberdashToWork(
+    withClients: [
+      MinimalPrintLumberdashClient(),
+    ],
+  );
+
+  BlocOverrides.runZoned(
+    () {
+      final vault = createVault(
+        isReleaseMode: kReleaseMode,
+      );
+
+      runApp(
+        OWWNCodingApp(
+          vault: vault,
+        ),
+      );
+    },
+    blocObserver: LogBlocObserver(),
+  );
 }
 
 class MyApp extends StatelessWidget {
