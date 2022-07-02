84b796de62098680bb9b23777539565babe336bf
freitzzz <1160907@isep.ipp.pt>
2022-07-02 22:41:47 +0100

    delete user error

--- lib/models/errors/errors.dart
+++ lib/models/errors/errors.dart
@@ -1,4 +1,3 @@
 export 'api_error.dart';
 export 'authentication_error.dart';
 export 'device_error.dart';
-export 'user_error.dart';
--- lib/models/errors/user_error.dart
+++
@@ -1,8 +0,0 @@
-import 'package:owwn_coding_challenge/data/data.dart';
-
-abstract class UserError extends RequestError {
-  const UserError({
-    required String cause,
-    required StackTrace stacktrace,
-  }) : super(cause: cause, stackTrace: stacktrace);
-}
