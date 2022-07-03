a0fd6408f1b0825bddfa6ad7a505c76b6febb7a3
freitzzz <1160907@isep.ipp.pt>
2022-07-04 00:46:20 +0100

    include statistics chart in user view

--- lib/presentation/views/users/user_view.dart
+++ lib/presentation/views/users/user_view.dart
@@ -1,5 +1,24 @@
-import 'package:owwn_coding_challenge/models/models.dart';
+import 'package:owwn_coding_challenge/blocs/blocs.dart';
 import 'package:owwn_coding_challenge/presentation/presentation.dart';
+
+const _userNameTextStyle = TextStyle(
+  fontSize: 28.0,
+);
+
+const _userEmailTextStyle = TextStyle(
+  color: Color(0xFFA7A7A7),
+  fontSize: 17.0,
+);
+
+const _statisticsNotAvailableTextStyle = TextStyle(
+  fontSize: 17.0,
+);
+
+const _statisticsChartPadding = EdgeInsets.symmetric(
+  horizontal: 25.0,
+);
+
+const _statisticsChartHeight = 121.0;
 
 class UserView extends StatelessWidget {
   const UserView({
@@ -8,37 +27,68 @@ class UserView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const user = User(
-      id: 'soheil@owwn.com',
-      name: 'Soheil',
-      email: 'soheil@owwn.com',
-      gender: Gender.male,
-      status: Status.active,
-      statistics: [],
-    );
+    final localizations = AppLocalizations.of(context);
+
+    final userBloc = context.read<UserBloc>();
 
     return Scaffold(
-      body: ListView(
-        primary: false,
-        children: [
-          const UserAvatar(
-            user: user,
-          ),
-          Text(
-            user.name,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontSize: 28.0,
-            ),
-          ),
-          Text(
-            user.email,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              fontSize: 17.0,
-            ),
-          ),
-        ],
+      appBar: AppBar(),
+      body: BlocBuilder<UserBloc, UserState>(
+        builder: (context, state) {
+          final user = state.user;
+
+          return ListView(
+            primary: false,
+            children: [
+              UserAvatar(
+                user: user,
+              ),
+              const SizedBox.square(
+                dimension: thirtyTwoPoints,
+              ),
+              Text(
+                user.name,
+                textAlign: TextAlign.center,
+                style: _userNameTextStyle,
+              ),
+              const SizedBox.square(
+                dimension: tenPoints,
+              ),
+              Text(
+                user.email,
+                textAlign: TextAlign.center,
+                style: _userEmailTextStyle,
+              ),
+              const SizedBox.square(
+                dimension: oneHundredPoints,
+              ),
+              if (user.hasStatistics) ...[
+                Padding(
+                  padding: _statisticsChartPadding,
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints.expand(
+                      height: _statisticsChartHeight,
+                    ),
+                    child: UserStatisticsLineChart(
+                      statistics: user.statistics,
+                    ),
+                  ),
+                ),
+                const SizedBox.square(
+                  dimension: eightPoints,
+                ),
+                const CustomPaint(
+                  painter: DashLinePainter(),
+                ),
+              ] else
+                Text(
+                  localizations.statisticsNotAvailable,
+                  style: _statisticsNotAvailableTextStyle,
+                  textAlign: TextAlign.center,
+                ),
+            ],
+          );
+        },
       ),
     );
   }
