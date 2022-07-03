9861d3719da0e6e2677e71b2ed2e8239c7a77d19
freitzzz <1160907@isep.ipp.pt>
2022-07-03 21:05:56 +0100

    updated dark theme to fit figma spec

--- lib/presentation/theme/theme.dart
+++ lib/presentation/theme/theme.dart
@@ -3,14 +3,16 @@ import 'package:flutter/material.dart';
 final _textThemeLight = ThemeData.light().textTheme;
 final _textThemeDark = ThemeData.dark().textTheme;
 
-const _seed = Color(0xFFDCEFD2);
+const _seed = Color(0xFF2E22F7);
 
 final _lightColorScheme = ColorScheme.fromSeed(
   seedColor: _seed,
+  background: Colors.white,
 );
 
 final _darkColorScheme = ColorScheme.fromSeed(
   seedColor: _seed,
+  background: Colors.black,
   brightness: Brightness.dark,
 );
 
@@ -18,10 +20,16 @@ final owwnCodingLightTheme = ThemeData(
   textTheme: _textThemeLight,
   useMaterial3: true,
   colorScheme: _lightColorScheme,
+  scaffoldBackgroundColor: _lightColorScheme.background,
 );
 
 final owwnCodingDarkTheme = ThemeData(
   textTheme: _textThemeDark,
   useMaterial3: true,
   colorScheme: _darkColorScheme,
+  scaffoldBackgroundColor: _darkColorScheme.background,
+  primaryColorDark: const Color(0xFF393939),
+  listTileTheme: const ListTileThemeData(
+    tileColor: Color(0xFF2B2B2B),
+  ),
 );
