650ee5c78db8a6d74a77c54ad6dc04b19cdfb449
freitzzz <1160907@isep.ipp.pt>
2022-07-03 22:50:44 +0100

    include gender icon in user avatar

--- lib/presentation/widgets/avatar.dart
+++ lib/presentation/widgets/avatar.dart
@@ -9,23 +9,62 @@ class UserAvatar extends StatelessWidget {
 
   final double radius;
 
+  final bool showGenderIcon;
+
   const UserAvatar({
     required this.user,
     this.radius = _profileAvatarRadius,
+    this.showGenderIcon = true,
     super.key,
   });
 
   const UserAvatar.tile({
     required final User user,
-  }) : this(user: user, radius: _tileAvatarRadius);
+  }) : this(
+          user: user,
+          radius: _tileAvatarRadius,
+          showGenderIcon: false,
+        );
 
   @override
   Widget build(BuildContext context) {
-    return CircleAvatar(
+    final avatar = CircleAvatar(
       radius: radius,
       child: Text(
         user.initials,
+        style: TextStyle(
+          fontSize: radius / 1.5,
+        ),
       ),
     );
+
+    if (showGenderIcon) {
+      return LayoutBuilder(
+        builder: (context, constraints) {
+          final theme = Theme.of(context);
+
+          return Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              avatar,
+              Positioned(
+                right: (constraints.maxWidth / 2) - radius,
+                child: CircleAvatar(
+                  backgroundColor: theme.colorScheme.onBackground,
+                  child: Center(
+                    child: Icon(
+                      user.gender.iconData,
+                      color: theme.colorScheme.background,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    } else {
+      return avatar;
+    }
   }
 }
