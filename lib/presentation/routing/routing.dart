2087f34ed245034804680dfd6ff72974a9416a08
freitzzz <1160907@isep.ipp.pt>
2022-07-03 22:01:04 +0100

    refactor routing navigator: defined app navigator widget that builds a Navigator widget defined of API to access app navigator state expose method on app navigator state that allows to set a new route

--- lib/presentation/routing/routing.dart
+++ lib/presentation/routing/routing.dart
@@ -3,13 +3,44 @@ import 'package:owwn_coding_challenge/core/core.dart';
 import 'package:owwn_coding_challenge/data/data.dart';
 import 'package:owwn_coding_challenge/presentation/presentation.dart';
 
-final appNavigator = Navigator(
-  pages: [
-    AuthenticationPage(),
-    UsersPage(),
-  ],
-  onPopPage: (route, result) => route.didPop(result),
-);
+const authenticationRoute = '/authentication';
+const usersRoute = '/users';
+
+class AppNavigator extends StatefulWidget {
+  const AppNavigator({
+    Key? key,
+  }) : super(key: key);
+
+  static AppNavigatorState of(final BuildContext context) {
+    return context.findAncestorStateOfType<AppNavigatorState>()!;
+  }
+
+  @override
+  State<AppNavigator> createState() => AppNavigatorState();
+}
+
+class AppNavigatorState extends State<AppNavigator> {
+  String route = authenticationRoute;
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      pages: [
+        if (route == authenticationRoute) AuthenticationPage(),
+        if (route == usersRoute) UsersPage(),
+      ],
+      onPopPage: (route, result) => route.didPop(result),
+    );
+  }
+
+  void setNewRoute(final String route) {
+    setState(
+      () {
+        this.route = route;
+      },
+    );
+  }
+}
 
 class AuthenticationPage extends MaterialPage {
   AuthenticationPage()
