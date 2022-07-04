8160bf05cf2d7e058349127bde1f27dee08fa9bc
freitzzz <1160907@isep.ipp.pt>
2022-07-04 03:21:25 +0100

    include owwn logo in assets

--- lib/presentation/widgets/images.dart
+++ lib/presentation/widgets/images.dart
@@ -1,6 +1,7 @@
 import 'package:owwn_coding_challenge/presentation/presentation.dart';
 
 const _usersCoverAssetImagePath = 'assets/images/users_cover.png';
+const _owwnLogoAssetImagePath = 'assets/images/owwn_logo.png';
 
 class UsersCover extends StatelessWidget {
   const UsersCover({
@@ -11,6 +12,20 @@ class UsersCover extends StatelessWidget {
   Widget build(BuildContext context) {
     return Image.asset(
       _usersCoverAssetImagePath,
+      fit: BoxFit.cover,
+    );
+  }
+}
+
+class OWWNLogo extends StatelessWidget {
+  const OWWNLogo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      _owwnLogoAssetImagePath,
       fit: BoxFit.cover,
     );
   }
